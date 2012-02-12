f4fa7a4581e8477a1b3454d54b2e733b32904034
Manuel Gómez <targen@gmail.com>
2012-02-12 01:52:22 -0430

    Menos archivos; toda la magia en el Makefile

--- tajadac/tdefs.cc
+++
@@ -1,5 +0,0 @@
-#include "tokens.hh"
-
-#define  TAJADA_TOKEN_BISON_DEFS(tag, description, regex, type) type|%token@<tag>@tag@description!
-TAJADA_TOKEN_DATA(TAJADA_TOKEN_BISON_DEFS)
-#undef TAJADA_TOKEN_BISON_DEFS
--- tajadac/utypes.cc
+++
@@ -1,5 +0,0 @@
-#include "tokens.hh"
-
-#define TAJADA_TOKEN_BISON_TYPES(tag, description, regex, type) type|type@tag;!
-TAJADA_TOKEN_DATA(TAJADA_TOKEN_BISON_TYPES)
-#undef TAJADA_TOKEN_BISON_TYPES
